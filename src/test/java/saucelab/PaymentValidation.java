7c091f0b5d8d8f7841bb29cb4be54e9addfa1ad6
KPPANDIYAN <petchimuthupandiyan.karuppasamy@gmail.com>
2026-07-15 22:45:08 +0530

    Branch Divergence Lab

--- src/test/java/saucelab/PaymentValidation.java
+++ src/test/java/saucelab/PaymentValidation.java
@@ -2,9 +2,13 @@ package saucelab;
 
 public class PaymentValidation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        System.out.println("Payment Validation");
+        System.out.println("Git Internals Learning");
+        System.out.println("Git Core Learning");
+        System.out.println("Adding a Branch divergence");
+    }
 
-	}
+
 
 }
